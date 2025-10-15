a0ecb3015afccbb5dc4e482db20965c7337048c5
Jason Colburne <j.colburne@gmail.com>
2025-10-14 21:33:33 -0300

    stronger typing

--- pkg/repository/interface.go
+++ pkg/repository/interface.go
@@ -2,9 +2,11 @@ package repository
 
 import (
 	"context"
+
+	"github.com/jasoncolburne/verifiable-storage-go/pkg/primitives"
 )
 
-type Repository[T any] interface {
+type Repository[T primitives.VerifiableAndRecordable] interface {
 	CreateVersion(ctx context.Context, record T) error
 	GetById(ctx context.Context, record T, id string) error
 	GetLatestByPrefix(ctx context.Context, record T, prefix string) error
