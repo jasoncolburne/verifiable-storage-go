ba8aae2467bce446f174f2dd0643cb04cf487984
Jason Colburne <j.colburne@gmail.com>
2025-10-14 21:27:38 -0300

    improve repository tests

--- pkg/repository/interface.go
+++ pkg/repository/interface.go
@@ -1,1 +1,12 @@
 package repository
+
+import (
+	"context"
+)
+
+type Repository[T any] interface {
+	CreateVersion(ctx context.Context, record T) error
+	GetById(ctx context.Context, record T, id string) error
+	GetLatestByPrefix(ctx context.Context, record T, prefix string) error
+	ListByPrefix(ctx context.Context, records *[]T, prefix string) error
+}
--- pkg/repository/repository_test.go
+++ pkg/repository/repository_test.go
@@ -12,6 +12,35 @@ import (
 	"github.com/jasoncolburne/verifiable-storage-go/pkg/repository"
 )
 
+type VerifiableModel struct {
+	primitives.VerifiableRecorder
+	Foo string `db:"foo" json:"foo"`
+	Bar string `db:"bar" json:"bar"`
+}
+
+func (*VerifiableModel) TableName() string {
+	return `verifiable`
+}
+
+var VERIFIABLE_TABLE_SQL = `
+CREATE TABLE IF NOT EXISTS verifiable (
+	-- Standard fields
+    id              	TEXT PRIMARY KEY,
+	prefix				TEXT NOT NULL,
+	previous        	TEXT,
+	sequence_number 	BIGINT NOT NULL,
+	created_at          DATETIME NOT NULL,
+	nonce           	TEXT NOT NULL,
+
+	-- Model-specific fields
+	foo 				TEXT NOT NULL,
+	bar                 TEXT NOT NULL,
+
+	-- Uniqueness constraint for sequence numbers
+	UNIQUE(prefix, sequence_number)
+);
+`
+
 type SignableModel struct {
 	primitives.SignableRecorder
 	Foo string `db:"foo" json:"foo"`
@@ -45,34 +74,91 @@ CREATE TABLE IF NOT EXISTS signable (
 );
 `
 
-func TestSignableRepository(t *testing.T) {
-	if err := exerciseSignableRepository(); err != nil {
+func TestVerifiableRepository(t *testing.T) {
+	repository, err := createVerifiableRepository()
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		t.FailNow()
+	}
+
+	record := &VerifiableModel{
+		Foo: "bar",
+		Bar: "baz",
+	}
+
+	buffers := []*VerifiableModel{{}, {}, {}}
+
+	if err := exerciseRepository(repository, record, buffers); err != nil {
 		fmt.Printf("%s\n", err)
 		t.Fail()
 	}
 }
 
-func exerciseSignableRepository() error {
+func createVerifiableRepository() (repository.Repository[*VerifiableModel], error) {
 	ctx := context.Background()
 
 	store, err := data.NewInMemorySQLiteStore()
 	if err != nil {
-		return err
+		return nil, err
+	}
+
+	_, err = store.Sql().ExecContext(ctx, VERIFIABLE_TABLE_SQL)
+	if err != nil {
+		return nil, err
+	}
+
+	noncer := examples.NewNoncer()
+
+	repository := repository.NewVerifiableRepository[*VerifiableModel](
+		store,
+		true,
+		noncer,
+	)
+
+	return repository, nil
+}
+
+func TestSignableRepository(t *testing.T) {
+	repository, err := createSignableRepository()
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		t.FailNow()
+	}
+
+	record := &SignableModel{
+		Foo: "bar",
+		Bar: "baz",
+	}
+
+	buffers := []*SignableModel{{}, {}, {}}
+
+	if err := exerciseRepository(repository, record, buffers); err != nil {
+		fmt.Printf("%s\n", err)
+		t.Fail()
+	}
+}
+
+func createSignableRepository() (repository.Repository[*SignableModel], error) {
+	ctx := context.Background()
+
+	store, err := data.NewInMemorySQLiteStore()
+	if err != nil {
+		return nil, err
 	}
 
 	_, err = store.Sql().ExecContext(ctx, SIGNABLE_TABLE_SQL)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	key, err := examples.NewEd25519(nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	keyIdentity, err := key.Identity()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	verificationKeyStore := examples.NewVerificationKeyStore()
@@ -88,10 +174,11 @@ func exerciseSignableRepository() error {
 		verificationKeyStore,
 	)
 
-	record2 := &SignableModel{
-		Foo: "bar",
-		Bar: "baz",
-	}
+	return repository, nil
+}
+
+func exerciseRepository[T primitives.VerifiableAndRecordable](repository repository.Repository[T], record2 T, buffers []T) error {
+	ctx := context.Background()
 
 	if err := repository.CreateVersion(ctx, record2); err != nil {
 		return err
@@ -101,66 +188,66 @@ func exerciseSignableRepository() error {
 		return err
 	}
 
-	id1 := record2.Id
+	id1 := record2.GetId()
 
 	if err := repository.CreateVersion(ctx, record2); err != nil {
 		return err
 	}
 
-	record1 := &SignableModel{}
+	record1 := buffers[0]
 	if err := repository.GetById(ctx, record1, id1); err != nil {
 		return err
 	}
 
-	record0 := &SignableModel{}
-	if err := repository.GetById(ctx, record0, record2.Prefix); err != nil {
+	record0 := buffers[1]
+	if err := repository.GetById(ctx, record0, record2.GetPrefix()); err != nil {
 		return err
 	}
 
-	latest := &SignableModel{}
-	if err := repository.GetLatestByPrefix(ctx, latest, record0.Id); err != nil {
+	latest := buffers[2]
+	if err := repository.GetLatestByPrefix(ctx, latest, record0.GetId()); err != nil {
 		return err
 	}
 
-	if record0.SequenceNumber != 0 {
-		return fmt.Errorf("unexpected sn for 0: %d", record0.SequenceNumber)
+	if record0.GetSequenceNumber() != 0 {
+		return fmt.Errorf("unexpected sn for 0: %d", record0.GetSequenceNumber())
 	}
 
-	if record0.Previous != nil {
+	if record0.GetPrevious() != nil {
 		return fmt.Errorf("previous not nil")
 	}
 
-	if !strings.EqualFold(record1.Prefix, record2.Prefix) {
+	if !strings.EqualFold(record1.GetPrefix(), record2.GetPrefix()) {
 		return fmt.Errorf("mismatched prefixes")
 	}
 
-	if strings.EqualFold(record1.Id, record2.Id) {
+	if strings.EqualFold(record1.GetId(), record2.GetId()) {
 		return fmt.Errorf("unexpected equal ids")
 	}
 
-	if record1.SequenceNumber != 1 {
-		return fmt.Errorf("unexpected sn for 1: %d", record1.SequenceNumber)
+	if record1.GetSequenceNumber() != 1 {
+		return fmt.Errorf("unexpected sn for 1: %d", record1.GetSequenceNumber())
 	}
 
-	if record2.SequenceNumber != 2 {
-		return fmt.Errorf("unexpected sn for 2: %d", record2.SequenceNumber)
+	if record2.GetSequenceNumber() != 2 {
+		return fmt.Errorf("unexpected sn for 2: %d", record2.GetSequenceNumber())
 	}
 
-	if record2.Previous == nil || !strings.EqualFold(*record2.Previous, record1.Id) {
+	if record2.GetPrevious() == nil || !strings.EqualFold(*record2.GetPrevious(), record1.GetId()) {
 		return fmt.Errorf("mismatched previous 1")
 	}
 
-	if record1.Previous == nil || !strings.EqualFold(*record1.Previous, record1.Prefix) {
+	if record1.GetPrevious() == nil || !strings.EqualFold(*record1.GetPrevious(), record1.GetPrefix()) {
 		return fmt.Errorf("mismatched previous 0")
 	}
 
-	if !strings.EqualFold(latest.Id, record2.Id) {
+	if !strings.EqualFold(latest.GetId(), record2.GetId()) {
 		return fmt.Errorf("latest id is mismatched")
 	}
 
-	records := []*SignableModel{}
+	records := []T{}
 
-	if err := repository.ListByPrefix(ctx, &records, record2.Prefix); err != nil {
+	if err := repository.ListByPrefix(ctx, &records, record2.GetPrefix()); err != nil {
 		return err
 	}
 
@@ -171,15 +258,15 @@ func exerciseSignableRepository() error {
 	for i, record := range records {
 		switch i {
 		case 0:
-			if !strings.EqualFold(record0.Id, record.Id) {
+			if !strings.EqualFold(record0.GetId(), record.GetId()) {
 				return fmt.Errorf("listed record 0 has incorrect id")
 			}
 		case 1:
-			if !strings.EqualFold(record1.Id, record.Id) {
+			if !strings.EqualFold(record1.GetId(), record.GetId()) {
 				return fmt.Errorf("listed record 1 has incorrect id")
 			}
 		case 2:
-			if !strings.EqualFold(record2.Id, record.Id) {
+			if !strings.EqualFold(record2.GetId(), record.GetId()) {
 				return fmt.Errorf("listed record 2 has incorrect id")
 			}
 		}
